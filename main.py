7c4f300ff6c3146bfa8f95bf2ed4509c7abdd9b8
sahilsharma17 <83007507+sahilsharma17@users.noreply.github.com>
2022-07-18 20:30:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from app_class import *
+from flask import *
+from threading import Thread
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
+
+answer = input('wrong')
---
+++ settings.py
@@ -0,0 +1,12 @@
+#screen settings
+WIDTH, HEIGHT = 448, 576  
+FPS = 60
+
+#colour setings
+BLACK = (0,0,0)
+
+#font settings
+START_TEXT_SIZE =16
+START_FONT = 'arial black'
+#player settings 
+#enenmy settings
